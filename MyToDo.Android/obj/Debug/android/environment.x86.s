a3a7b1943c08ac979d0a400bea9229c74c22212b
yahyamokaied <yahyamokaied@gmail.com>
2020-11-05 13:38:20 +0100

    new commit

--- MyToDo.Android/obj/Debug/android/environment.x86.s
+++ MyToDo.Android/obj/Debug/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"90574dde-73b6-4e4d-afa9-eee75ecb9c70"
+	.asciz	"0f56a8ae-8f4a-4771-90ea-245b004f42e3"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
